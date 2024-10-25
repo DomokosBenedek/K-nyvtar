4b1d7fecb612a20c46da1af5af8006f5a692ea72
Domokos Benedek <benedekdomokos50@gmail.com>
2024-10-25 15:49:35 +0200

    update_id_error

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class CreateBookDto {
+    @IsString()
+    @IsNotEmpty()
     title: string;
+    @IsString()
+    @IsNotEmpty()
     author: string;
+    @IsString()
+    @IsNotEmpty()
     isbn: string;
+    @IsNumber()
+    @IsNotEmpty()
     publishYear: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,11 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }))
   
   await app.listen(3000);
 }
